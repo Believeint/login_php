b185961f9b80097a6f2f06f24e852aef880c7194
elias.santos <elias.carvalho.096@gmail.com>
2019-12-14 21:34:57 -0300

    Config Helper Class

--- Model/Config.php
+++ Model/Config.php
@@ -3,5 +3,20 @@
 
 class Config
 {
+    public static function get($path)
+    {
+        if($path) {
+            $config = $GLOBALS['config'];
+            $path = explode("/", $path);
+
+            foreach ($path as $bit) {
+                if(isset($config[$bit])){
+                    $config = $config[$bit];
+                }
+            }
+            return $config;
+        }
+        return false;
+    }
 
 }
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
 require_once 'Core/init.php';
+
+echo Config::get('mysql/host'); // '127.0.0.1'
